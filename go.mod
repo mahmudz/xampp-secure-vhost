f7c9b83a8270bda1c2fc02c4a06fa79e67edca4f
Mahmudul Hasan <mahmudbappy.pri@gmail.com>
2021-02-17 23:17:29 +0600

    Mkcert downloader added via github api

---
+++ go.mod
@@ -0,0 +1,5 @@
+module xampp-secure-vhost
+
+go 1.15
+
+require github.com/dustin/go-humanize v1.0.0
--- main.go
+++ main.go
@@ -1,54 +1,136 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"github.com/dustin/go-humanize"
+	"io"
+	"log"
 	"net/http"
+	"os"
+	"os/exec"
+	"os/user"
+	"strings"
+	"sync"
 )
 
+type WriteCounter struct {
+	Total uint64
+}
+
 type RepoReleases struct {
-	Array []RepoRelease
+	Collection []RepoRelease
 }
 
 type RepoRelease struct {
-	url    string             `json:"url"`
-	assets []RepoReleaseAsset `json:"assets"`
+	Url    string             `json:"url"`
+	Assets []RepoReleaseAsset `json:"assets"`
 }
 
 type RepoReleaseAsset struct {
-	browserDownloadUrl string `json:"browser_download_url"`
+	BrowserDownloadUrl string `json:"browser_download_url"`
 }
 
-func check(e error, msg string) {
-	if e != nil {
-		if msg != "" {
-			fmt.Println(msg)
-		} else {
-			panic(e)
-		}
-	}
+type XamppSecureVhost struct {
+	domainName, projectRoot string
 }
 
-func prepareMkcert() bool {
-	res, _ := http.Get("https://api.github.com/repos/FiloSottile/mkcert/releases")
-	defer res.Body.Close()
+func (wc WriteCounter) PrintProgress() {
+	fmt.Printf("\r%s", strings.Repeat(" ", 50))
 
-	body, _ := ioutil.ReadAll(res.Body)
+	fmt.Printf("\rDownloading mkcert binary... %s", humanize.Bytes(wc.Total))
+}
 
-	var arbitrary_json map[string]interface{}
-	json.Unmarshal([]byte(body), &arbitrary_json)
+func (wc *WriteCounter) Write(p []byte) (int, error) {
+	n := len(p)
+	wc.Total += uint64(n)
+	wc.PrintProgress()
+	return n, nil
+}
 
-	for key, val := range arbitrary_json {
-		fmt.Print(key, val)
+func DownloadFile(url string, filepath string) error {
+	out, err := os.Create(filepath + ".tmp")
+	if err != nil {
+		return err
 	}
+	defer out.Close()
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	counter := &WriteCounter{}
+	_, err = io.Copy(out, io.TeeReader(resp.Body, counter))
+	if err != nil {
+		return err
+	}
+
+	fmt.Println()
+
+	err = os.Rename(filepath+".tmp", filepath)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func binaryExists(name string) bool {
+	_, err := exec.LookPath(name)
+	return err == nil
+}
+
+var sudoWarningOnce sync.Once
+
+func commandWithSudo(cmd ...string) *exec.Cmd {
+	if u, err := user.Current(); err == nil && u.Uid == "0" {
+		return exec.Command(cmd[0], cmd[1:]...)
+	}
+	if !binaryExists("sudo") {
+		sudoWarningOnce.Do(func() {
+			log.Println(`Warning: "sudo" is not available, and mkcert is not running as root. The (un)install operation might fail. ⚠️`)
+		})
+		return exec.Command(cmd[0], cmd[1:]...)
+	}
+	return exec.Command("sudo", append([]string{"--prompt=Sudo password:", "--"}, cmd...)...)
+}
+
+func PrepareMkcert() bool {
+	//osType := runtime.GOOS
+
+	stores := os.Getenv("TRUST_STORES")
+
+	fmt.Println(stores)
+
+	_, err := exec.LookPath("./mkcert")
+	panic(err)
+	//if err != nil {
+	//	res, _ := http.Get("https://api.github.com/repos/FiloSottile/mkcert/releases")
+	//	defer res.Body.Close()
+	//
+	//	body, _ := ioutil.ReadAll(res.Body)
+	//	releases := make([]RepoRelease, 0)
+	//
+	//	json.Unmarshal([]byte(body), &releases)
+	//	if len(releases) > 0 {
+	//		latestRelease := releases[0]
+	//		for _, asset := range latestRelease.Assets{
+	//
+	//			if strings.Contains(asset.BrowserDownloadUrl, osType) {
+	//				DownloadFile(asset.BrowserDownloadUrl, "./mkcert")
+	//				break
+	//			}
+	//		}
+	//	}
+	//}
 
 	return true
 }
 
 func main() {
 
-	prepareMkcert()
+	PrepareMkcert()
 
 	//	var hostName string
 	//	var rootPath string
